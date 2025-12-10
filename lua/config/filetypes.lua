7227cdaff09483c6dfcbfb3d92b19f9964411c71
Oleksandr Manenko <koornacht@use.startmail.com>
2025-12-10 17:28:52 +0100

    Upade OCaml textwidth config

--- lua/config/filetypes.lua
+++ lua/config/filetypes.lua
@@ -4,10 +4,11 @@
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "ocaml",
     callback = function()
-        vim.opt_local.tabstop    = 2
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.expandtab  = true
-        vim.opt_local.textwidth  = 80
+        vim.opt_local.tabstop     = 2
+        vim.opt_local.shiftwidth  = 2
+        vim.opt_local.expandtab   = true
+        vim.opt_local.textwidth   = 80
+        vim.opt_local.colorcolumn = "80"
     end,
 })
 
@@ -15,9 +16,10 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "ocamlinterface",
     callback = function()
-        vim.opt_local.tabstop    = 2
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.expandtab  = true
-        vim.opt_local.textwidth  = 80
+        vim.opt_local.tabstop     = 2
+        vim.opt_local.shiftwidth  = 2
+        vim.opt_local.expandtab   = true
+        vim.opt_local.textwidth   = 80
+        vim.opt_local.colorcolumn = "80"
     end,
 })
